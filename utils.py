27f142533c931dbabfb9e18abfc311c2536010c4
prl <song001031@163.com>
2024-01-30 15:56:56 -0800

    add network file & fix bugs

--- utils.py
+++ utils.py
@@ -57,6 +57,6 @@ def accuracy(output, labels, topk=(1,2)):
 
         res = []
         for k in topk:
-            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            correct_k = correct[:k].contiguous().view(-1).float().sum(0, keepdim=True)
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
